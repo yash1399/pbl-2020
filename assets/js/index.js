8ca2b24d2b90281ce95bb77e96de45c8e11a9c3e
Yash <bavaskaryash1399@gmail.com>
2020-10-16 02:07:33 +0530

    List function

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+
+
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+
+
+
+$("ul").on("click","span",function(event){
+  $(this).parent().fadeOut().remove(3000, function(){
+    $(this).remove();
+  })
+  event.stopPropagation(); 
+});
+
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13 ){ 
+    var itemCreation = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + itemCreation +"</li>");
+  }
+})
+
+$(".fa-plus").click(function(){ 
+  $("input[type='text']").fadeToggle(500);
+})
